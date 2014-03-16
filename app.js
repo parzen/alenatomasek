7f98c01ce092a9343676bc83657b2874ac88ea75
Adrian Tomasek <adrian.tomasek@gmail.com>
2014-03-16 15:24:27 +0100

    bla

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-var express = require("express");
-var app = express();
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var express   = require("express");
+var app       = express();
 var config = require('./config/config');
 
 require('./init/express')(app, config);
 require('./init/routes')(app);
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World, this is me!');
-// });
+var WebSocketServer = require('ws').Server
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port ' + port);
 });
+
+wss = new WebSocketServer({
+    server: app,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
