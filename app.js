ebac319be2cb488eb12361776156ec18cb722c67
Adrian Tomasek <adrian.tomasek@gmail.com>
2014-03-17 18:35:40 +0100

    removed ws

--- app.js
+++ app.js
@@ -2,27 +2,20 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var express   = require("express");
 var app       = express();
-var config = require('./config/config');
+var config    = require('./config/config');
+var mongoose  = require('mongoose');
 
 require('./init/express')(app, config);
 require('./init/routes')(app);
-
-var WebSocketServer = require('ws').Server
 
 app.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port ' + port);
 });
 
-wss = new WebSocketServer({
-    server: app,
-    autoAcceptConnections: false
-});
-wss.on('connection', function(ws) {
-  console.log("New connection");
-  ws.on('message', function(message) {
-    ws.send("Received: " + message);
-  });
-  ws.send('Welcome!');
+mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', function() {
+	throw new Error('unable to connect to database at ' + config.db);
 });
 
 console.log("Listening to " + ipaddress + ":" + port + "...");
