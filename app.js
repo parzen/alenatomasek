7bef07107de26c5b3bfe7bd73a62a4e0542d007a
Tomasek <tms@iis.fhg.de>
2017-09-27 20:58:11 +0100

    prepare for v3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = '0.0.0.0' || "127.0.0.1";
+var port      = 8080;
 var express   = require("express");
 var app       = express();
 var config    = require('./config/config');
